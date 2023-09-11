898f8bd5ba90c882c95ba7887d2744f895107a79
Hasan-abu-khalil <hasan.aak1998@gmail.com>
2023-09-11 09:27:27 +0300

    hasan

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,12 +7,19 @@
 @extends('layouts.master')
 
 
-@section('title','Home')
+@section('title','about')
 
 
 
 
-
+<div class="site-mobile-menu site-navbar-target">
+		<div class="site-mobile-menu-header">
+			<div class="site-mobile-menu-close">
+				<span class="icofont-close js-menu-toggle"></span>
+			</div>
+		</div>
+		<div class="site-mobile-menu-body"></div>
+	</div>
 
 
 
