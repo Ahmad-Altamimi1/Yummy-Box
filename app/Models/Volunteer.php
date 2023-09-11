97c180d04eaea51603f99c8f229ab2e4ebedd3fc
Ahmad-Altamimi1 <135625071+Ahmad-Altamimi1@users.noreply.github.com>
2023-09-11 11:41:18 +0300

    tmtm

--- app/Models/Volunteer.php
+++ app/Models/Volunteer.php
@@ -10,15 +10,5 @@ class Volunteer extends Model
 {
     use HasFactory;
     protected $table = 'volunteers';
-<<<<<<< HEAD
-    protected $fillable = ['user_id'=>"1",'Address', 'Languages' , 'Experience' , 'CV',];
-    public function users()
-    {
-        
-        return $this->hasOne(User::class);
-
-    }
-=======
     protected $fillable = ['Address', 'Languages' , 'day' , 'Experience' , 'CV'];
->>>>>>> f3afae06a2528f96e07427878c2c68daae02d416
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -99,16 +99,7 @@
 
 						<input type="submit" value="Donate by paypal" class="btn w-100" style="background:url('images/PayPal-Logo.png')">
                 </form>
-                                    <form action="stripe" class="bg-white p-5 rounded donation-form" method="post">
-@csrf
-                        <h3 for="" style="background: white; margin:0;    font-weight: bold;
-    margin-bottom: 0px;
-    text-transform: uppercase;
-    font-size: 18px;">Or donate by Visa</h3>
-    
-                         <input type="text" placeholder="0.00" class="form-control px-4" name="price">
-                         <input type="submit" value="visa" class="btn visa w-100" style="" >
-                    </form>
+     
 
 
 
