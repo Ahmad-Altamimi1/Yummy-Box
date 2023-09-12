481c3534d660a1c9690d249789b30a589cf85e56
Sajeda-Momani <sajeda.momani20@gmail.com>
2023-09-13 00:08:33 +0300

    sajeda

--- resources/views/Admin_Dashboard/Project_Create.blade.php
+++ resources/views/Admin_Dashboard/Project_Create.blade.php
@@ -1,5 +1,3 @@
-
-      
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,56 +15,55 @@
             <div class="card-header">
                 <h5 class="card-title">Add New Project</h5>
             </div>
-            <div class="card-body">          
-              <form action="productupdate/{{ $product->id }}" method="POST">
-              @csrf
-              @method('PATCH') 
-              
-
+            <div class="card-body">
+                <form action="Admins_Projects" method="POST" enctype="multipart/form-data">
+                    @csrf
                     <div class="form-group">
                         <label for="ProjectsName">Name</label>
-                        <input type="text" class="form-control" id="ProjectsName" name="name"  value="{{ $product->name }}">
-                    </div>  
+                        <input type="text" class="form-control" id="ProjectsName" name="name">
+                    </div>
                     <div class="form-group">
                         <label for="ProjectsBreif">Brief</label>
-                        <input type="text" class="form-control" id="ProjectsBreif" name="breif" value="{{ $product->breif }}">
+                        <input type="text" class="form-control" id="ProjectsBreif" name="breif">
                     </div>
                     <div class="form-group">
                         <label for="DescriptionOne">Description</label>
-                        <input type="text" class="form-control" id="DescriptionOne" name="description2"  value="{{ $product->description2 }}">
+                        <input type="text" class="form-control" id="DescriptionOne" name="description2">
                     </div>
                     <div class="form-group">
                         <label for="DescriptionTwo">Details</label>
-                        <input type="text" class="form-control" id="DescriptionTwo" name="description3" value="{{ $product->description3 }}">
+                        <input type="text" class="form-control" id="DescriptionTwo" name="description3">
                     </div>
                     <div class="form-group">
                         <label for="ProjectsLocation">Location</label>
-                        <input type="text" class="form-control" id="ProjectsLocation" name="location"  value="{{ $product->location }}">
+                        <input type="text" class="form-control" id="ProjectsLocation" name="location">
                     </div>
                     <div class="form-group">
                         <label for="ProjectsTime">Time</label>
-                        <input type="text" class="form-control" id="ProjectsTime" name="time" value="{{ $product->time }}">
+                        <input type="text" class="form-control" id="ProjectsTime" name="time">
                     </div>
                     <div class="form-group">
                         <label for="ProjectsPeriod">Period</label>
-                        <input type="text" class="form-control" id="ProjectsPeriod" name="period"  value="{{ $product->period }}">
+                        <input type="text" class="form-control" id="ProjectsPeriod" name="period">
                     </div>
                     <div class="form-group">
                         <label for="ProjectsTotal">Total</label>
-                        <input type="text" class="form-control" id="ProjectsTotal" name="total" value="{{ $product->total }}">
+                        <input type="text" class="form-control" id="ProjectsTotal" name="total">
                     </div>
+                    {{-- <div class="form-group">
+                        <label for="ProjectsImage">Image</label>
+                        <input type="file" class="form-control-file" id="ProjectsImage" name="image">
+                    </div> --}}
                     <div class="col-md-4 col-12">
-                      @if ($product->image)
-                          <img src="{{ asset('images/users/' . $product->image) }}" alt="{{ $product->name }}'s Project Picture" class="img-fluid" style="max-width: 200px; height: auto;" requ>
-                      @endif
-      
-                      <div class="form-group mt-3">
-                          <label for="image">{{ __('Upload new image') }}</label>
-                          <input id="image" name="image" type="file" accept="image/*" class="form-control-file" :value="old('image', $product->image)" autocomplete="image" />
-                          <x-input-error class="mt-2" :messages="$errors->get('image')" />
-                      </div>
-                  </div>
-                    <button type="submit" class="btn btn-primary"> Update Project</button>
+                        <div class="form-group mt-3">
+                            <label for="image">{{ __('Upload new image') }}</label>
+                            <input id="image" name="image" type="file" accept="image/*" class="form-control-file" autocomplete="image" />
+                            <x-input-error class="mt-2" :messages="$errors->get('image')" />
+                        </div>
+                    </div>
+                    
+                    
+                    <button type="submit" class="btn btn-primary">Add Project</button>
                 </form>
             </div>
         </div>
