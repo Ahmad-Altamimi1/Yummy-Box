12f09eb1816fa777bc46b9c289e5bcdddcafbc9a
ahmadkhatatbe <ahmedkatatbeh123@gmail.comgit config --global user.email ahmedkatatbeh123@gmail.com>
2023-09-13 23:12:58 +0300

    Co-authored-by: Ahmad-Altamimi1 <135625071+Ahmad-Altamimi1@users.noreply.github.com>

--- resources/views/pages/frontendForm.blade.php
+++ resources/views/pages/frontendForm.blade.php
@@ -1,41 +1,41 @@
-
 @include('layouts.nav')
 
-    
 
-    @section('content')  
-    
 
-      
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+@section('content')
+
+
+
+    <link rel="stylesheet" href="{{ url('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css') }}">
     <style>
-h1 {
-    text-align: center;
-    margin-top:120px;
-}
+        h1 {
+            text-align: center;
+            margin-top: 120px;
+        }
 
-.form-group label {
-    font-weight: bold;
-}
+        .form-group label {
+            font-weight: bold;
+        }
 
-.btn-primary {
-    background-color: #007bff;
-    border-color: #007bff;
-}
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
 
-.btn-primary:hover {
-    background-color: #0056b3;
-    border-color: #0056b3;
-}
+        .btn-primary:hover {
+            background-color: #0056b3;
+            border-color: #0056b3;
+        }
 
-.btn-primary:focus {
-    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.5);
-}
-button{
-margin-bottom: 20px;
-width: 200px;
+        .btn-primary:focus {
+            box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.5);
+        }
 
-}
+        button {
+            margin-bottom: 20px;
+            width: 200px;
+
+        }
     </style>
     <div class="container mt-5">
         <h1>Frontend Training Volunteer Form</h1>
@@ -43,25 +43,25 @@ width: 200px;
             @csrf
             @method('post')
             <div>
-                    <label for="firstName">Name : </label>
-                    <label for="firstName">{{Auth::user()->name}}</label>
-                </div>   
-                
-                <div>
-                    <label for="email">Email : </label>
-                    <label for="email">{{Auth::user()->email}}</label>
-                </div>   
-               
-            
-            
-                
-                <div>
-                    <label for="phoneNumber">Phone Number</label>
-                    <label for="phoneNumber">{{Auth::user()->phone}}</label>
-                </div> 
+                <label for="firstName">Name : </label>
+                <label for="firstName">{{ Auth::user()->name }}</label>
+            </div>
 
-            <div class="form-group">                
-                <input  type="hidden" class="form-control" id="user_id"  name="user_id" required>
+            <div>
+                <label for="email">Email : </label>
+                <label for="email">{{ Auth::user()->email }}</label>
+            </div>
+
+
+
+
+            <div>
+                <label for="phoneNumber">Phone Number</label>
+                <label for="phoneNumber">{{ Auth::user()->phone }}</label>
+            </div>
+
+            <div class="form-group">
+                <input type="hidden" class="form-control" id="user_id" name="user_id" required>
 
                 <label for="address">Address</label>
                 <input type="text" class="form-control" id="address" name="Address" required>
@@ -70,7 +70,7 @@ width: 200px;
             </div>
             <div class="form-group">
                 <label for="programmingLanguages">Proficient Frontend Programming Languages</label>
-                <select class="form-control" id="programmingLanguages" name="Languages" >
+                <select class="form-control" id="programmingLanguages" name="Languages">
                     <option value="Choose Language">Choose Language</option>
                     <option value="HTML">HTML</option>
                     <option value="CSS">CSS</option>
@@ -81,7 +81,7 @@ width: 200px;
             </div>
             <div class="form-group">
                 <label for="day">I would like to help weekly</label>
-                <select class="form-control" id="day" name="day" >
+                <select class="form-control" id="day" name="day">
                     <option value="Choose Day">Choose Day</option>
                     <option value="saturday">Saturday</option>
                     <option value="sunday">Sunday</option>
@@ -89,7 +89,7 @@ width: 200px;
                     <option value="tuesday">Tuesday</option>
                     <option value="wednesday">Wednesday</option>
                     <option value="thursday">Thursday</option>
-                    
+
                 </select>
             </div>
             <div class="form-group">
@@ -100,23 +100,23 @@ width: 200px;
                 <label for="cv">CV</label>
                 <input type="file" class="form-control-file" id="cv" name="CV" required>
             </div>
-         @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-            
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
             <button type="submit" class="btn btn-primary  " value="submit">Submit</button>
         </form>
-        
+
     </div>
 
 
 
 
 
-@include('layouts.footer')
+    @include('layouts.footer')
